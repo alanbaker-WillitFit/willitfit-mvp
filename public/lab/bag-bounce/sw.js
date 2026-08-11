5d5c7906d5ae7afbbd574d6d7570972f6433ea15
alanbaker-WillitFit <alanbaker.ab10@gmail.com>
2026-08-11 12:31:15 +0100

    advance Bag Bounce long-loop offline cache

--- public/lab/bag-bounce/sw.js
+++ public/lab/bag-bounce/sw.js
@@ -1,4 +1,4 @@
-const CACHE="willitlab-bag-bounce-rc1";
+const CACHE="willitlab-bag-bounce-rc1-longloop-1";
 const ASSETS=["./","./index.html","./styles.css","./asset-refs.js","./game.js","./manifest.webmanifest"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
