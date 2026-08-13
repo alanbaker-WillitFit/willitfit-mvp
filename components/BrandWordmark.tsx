4f5d0afe39c1f94ec8fadfc7edde71987fc8f01d
alanbaker-WillitFit <alanbaker.ab10@gmail.com>
2026-08-13 20:30:42 +0100

    cleanse(willitfly): remove legacy generic brand wordmark

--- components/BrandWordmark.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function BrandWordmark({ className = "" }: { className?: string }) {
-  return <span className={`wf-wordmark ${className}`}>Will<span>it</span>Fit</span>;
-}
