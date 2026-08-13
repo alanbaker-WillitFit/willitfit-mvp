b23251fed39c5fe76ccf37ed209b7d49660c9b53
alanbaker-WillitFit <alanbaker.ab10@gmail.com>
2026-08-13 20:35:35 +0100

    cleanse(willitfly): remove legacy generic utility helper

--- lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function cn(...classes: Array<string | false | null | undefined>): string {
-  return classes.filter(Boolean).join(" ");
-}
-
-export function siteUrl(path = ""): string {
-  const base = process.env.NEXT_PUBLIC_SITE_URL ?? "https://will-it-fit.net";
-  return `${base.replace(/\/$/, "")}${path}`;
-}
